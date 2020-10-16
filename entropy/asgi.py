6d746b76eef937f524b0c7e17d4918f4de608dca
Aleksei Khatkevich <alekseikhatkevich@gmail.com>
2020-10-16 12:29:36 +0300

    added django-configurations support for asgi

--- entropy/asgi.py
+++ entropy/asgi.py
@@ -25,7 +25,7 @@ try:
 except ImportError:
     from django.core.handlers.asgi import ASGIHandler
 
-    def get_asgi_application():  
+    def get_asgi_application():
         return ASGIHandler()
 
 application = get_asgi_application()
