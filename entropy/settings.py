b25e8e8fd0006529b9de02147394c06d10cb7292
Aleksei Khatkevich <alekseikhatkevich@gmail.com>
2020-10-17 12:43:08 +0300

    save before dynaconf

--- entropy/settings.py
+++ entropy/settings.py
@@ -142,9 +142,17 @@ class Docker():
     """
     Configuration class for use in Docker.
     """
+
     @property
     def INTERNAL_IPS(self):
         hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 
         return [ip[:-1] + "1" for ip in ips]
 
+
+# HERE STARTS DYNACONF EXTENSION LOAD (Keep at the very bottom of settings.py)
+# Read more at https://dynaconf.readthedocs.io/en/latest/guides/django.html
+import dynaconf  # noqa
+
+settings = dynaconf.DjangoDynaconf(__name__)  # noqa
+# HERE ENDS DYNACONF EXTENSION LOAD (No more code below this line)
